1ffa018b1d8dbbd20b0b46baa3fdb8283d5f4fc8
Ángel López Martínez <angelserilopezmartinez@gmail.com>
2026-04-21 11:50:11 -0600

    modificacion manual crowdfounding 1.3.2.7.4

--- src/routes/campanasAdmin.routes.js
+++ src/routes/campanasAdmin.routes.js
@@ -1,32 +1,21 @@
 const { Router } = require('express')
 const {
   obtenerCampanas,
-  obtenerCampanasPublicas,
-  obtenerCampanaPorId,
   crearCampana,
   actualizarCampana,
-  actualizarEstadoCampana,
   toggleActivoCampana,
-  obtenerMisCampanas,
-  publicarActualizacion,
-  obtenerActualizaciones
-} = require('../controllers/campanas.controller')
-const { verificarToken } = require('../middlewares/auth.middleware')
+  obtenerNegociosParaSelect
+} = require('../controllers/campanasAdmin.controller')
+const { verificarToken, esAdmin } = require('../middlewares/auth.middleware')
 
 const router = Router()
 
-router.get('/publicas', obtenerCampanasPublicas)
-router.get('/:id/actualizaciones', obtenerActualizaciones)
+router.use(verificarToken, esAdmin)
 
-router.use(verificarToken)
-
-router.get('/mis-campanas', obtenerMisCampanas)
+router.get('/negocios/opciones', obtenerNegociosParaSelect)
 router.get('/', obtenerCampanas)
-router.get('/:id', obtenerCampanaPorId)
 router.post('/', crearCampana)
 router.put('/:id', actualizarCampana)
-router.patch('/:id/estado', actualizarEstadoCampana)
 router.patch('/:id/toggle-activo', toggleActivoCampana)
-router.post('/:id/actualizaciones', publicarActualizacion)
 
 module.exports = router
